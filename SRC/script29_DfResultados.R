aaaced8e76287c630068ca338659376b1d5e7504
Larissa Sayuri Futino Castro dos Santos <44413674+larissasayurifutino@users.noreply.github.com>
2022-02-06 11:07:49 -0300

    Removing code written in PT.

--- SRC/script29_DfResultados.R
+++
@@ -1,7 +0,0 @@
-cam <- "/media/leste/5B8F-C6B3/06_AplicacaoSentimento/01_Bases/resClassif"
-arqs <- list.files(cam)
-
-lista <- lapply(arqs, FUN = function(x){load(file.path(cam, x)); return(resultadoFinal)})
-dfResultados <- do.call(rbind, lista)
-
-save(dfResultados, file = file.path("/media/leste/5B8F-C6B3/06_AplicacaoSentimento/01_Bases", "dfResultados.RData"))
